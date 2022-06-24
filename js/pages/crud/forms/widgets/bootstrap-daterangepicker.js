1e87c2485673b8d9cbc8950d7a033358267b8bd2
liuzhan <liuzhan@topgamesinc.com>
2022-06-24 17:48:38 +0800

    feat: 初始化“

---
+++ js/pages/crud/forms/widgets/bootstrap-daterangepicker.js
@@ -0,0 +1,144 @@
+// Class definition
+
+var KTBootstrapDaterangepicker = function () {
+
+    // Private functions
+    var demos = function () {
+        // minimum setup
+        $('#kt_daterangepicker_1, #kt_daterangepicker_1_modal').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        });
+
+        // input group and left alignment setup
+        $('#kt_daterangepicker_2').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_2 .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+
+         $('#kt_daterangepicker_2_modal').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_2 .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+
+        // left alignment setup
+        $('#kt_daterangepicker_3').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_3 .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+
+        $('#kt_daterangepicker_3_modal').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_3 .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+
+
+        // date & time
+        $('#kt_daterangepicker_4').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary',
+
+            timePicker: true,
+            timePickerIncrement: 30,
+            locale: {
+                format: 'MM/DD/YYYY h:mm A'
+            }
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_4 .form-control').val( start.format('MM/DD/YYYY h:mm A') + ' / ' + end.format('MM/DD/YYYY h:mm A'));
+        });
+
+        // date picker
+        $('#kt_daterangepicker_5').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary',
+
+            singleDatePicker: true,
+            showDropdowns: true,
+            locale: {
+                format: 'MM/DD/YYYY'
+            }
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_5 .form-control').val( start.format('MM/DD/YYYY') + ' / ' + end.format('MM/DD/YYYY'));
+        });
+
+        // predefined ranges
+        var start = moment().subtract(29, 'days');
+        var end = moment();
+
+        $('#kt_daterangepicker_6').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary',
+
+            startDate: start,
+            endDate: end,
+            ranges: {
+                '今天': [moment(), moment()],
+                '昨天': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                '最近 7 天': [moment().subtract(6, 'days'), moment()],
+                '最近 30 天': [moment().subtract(29, 'days'), moment()],
+                '本月': [moment().startOf('month'), moment().endOf('month')],
+                '上个月': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+                '所有时间': [0,moment()]
+            }
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_6 .form-control').val( start.format('MM/DD/YYYY') + ' / ' + end.format('MM/DD/YYYY'));
+        });
+    }
+
+    var validationDemos = function() {
+        // input group and left alignment setup
+        $('#kt_daterangepicker_1_validate').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_1_validate .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+
+        // input group and left alignment setup
+        $('#kt_daterangepicker_2_validate').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_3_validate .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+
+        // input group and left alignment setup
+        $('#kt_daterangepicker_3_validate').daterangepicker({
+            buttonClasses: ' btn',
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-secondary'
+        }, function(start, end, label) {
+            $('#kt_daterangepicker_3_validate .form-control').val( start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        });
+    }
+
+    return {
+        // public functions
+        init: function() {
+            demos();
+            validationDemos();
+        }
+    };
+}();
+
+jQuery(document).ready(function() {
+    KTBootstrapDaterangepicker.init();
+});
